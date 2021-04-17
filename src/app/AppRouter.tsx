2ecfd46114474dc85261aedaa72873464c8156da
Oscar <oscargelvez23@gmail.com>
2021-04-16 19:38:01 -0500

    se crea conflicto 2

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -3,15 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { HomeRouter } from 'app/feature/Home/HomeRouter';
 import MainPage from 'app/Main';
 import { NavigationHeader } from 'app/shared/components/NavigationHeader';
-import { ProductoRouter } from 'app/feature/Producto/ProductoRouter';
+// import { ProductoRouter } from 'app/feature/Producto/ProductoRouter';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <NavigationHeader />
       <Switch>
+        <Route path="/productos" component={HomeRouter} />
         <Route path="/home" component={HomeRouter} />
-        <Route path="/productos" component={ProductoRouter} />
         <Route path="/" component={MainPage} />
       </Switch>
     </BrowserRouter>
