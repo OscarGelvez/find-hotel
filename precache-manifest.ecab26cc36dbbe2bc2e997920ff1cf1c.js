26225d8b70b38c4992aa3ad16711fc6b5fb374f9
OscarGelvez <oscargelvez23@gmail.com>
2021-04-18 21:12:00 +0000

    Updates

---
+++ precache-manifest.ecab26cc36dbbe2bc2e997920ff1cf1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fcc6c090f80c9068ecb2ecfd260253",
+    "url": "/find-hotel/index.html"
+  },
+  {
+    "revision": "06657f49fa9b2e41dc54",
+    "url": "/find-hotel/static/css/main.d4d656fe.chunk.css"
+  },
+  {
+    "revision": "039462bc0c3d9996573e",
+    "url": "/find-hotel/static/js/0.97dc82d4.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/find-hotel/static/js/0.97dc82d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ea052e3af930b2a881",
+    "url": "/find-hotel/static/js/3.b872dc60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/find-hotel/static/js/3.b872dc60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef3dd759933c04a4c3d",
+    "url": "/find-hotel/static/js/4.196f8d2d.chunk.js"
+  },
+  {
+    "revision": "8490cf535e69673a3562",
+    "url": "/find-hotel/static/js/5.43c44c31.chunk.js"
+  },
+  {
+    "revision": "e5de61817e774eaf2fd2",
+    "url": "/find-hotel/static/js/6.708807db.chunk.js"
+  },
+  {
+    "revision": "06657f49fa9b2e41dc54",
+    "url": "/find-hotel/static/js/main.1855274b.chunk.js"
+  },
+  {
+    "revision": "6c396338c5697d425b2f",
+    "url": "/find-hotel/static/js/runtime-main.7781c10a.js"
+  },
+  {
+    "revision": "1e0431edbc7a75798fe93df925d292eb",
+    "url": "/find-hotel/static/media/logoHotel.1e0431ed.png"
+  }
+]);
