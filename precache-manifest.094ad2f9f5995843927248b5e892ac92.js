6d424b2e6891fbf5a62c853ea12f49175ce44f27
Oscar Gelvez <oscargelvez23@gmail.com>
2021-04-19 10:53:13 -0500

    add new build

---
+++ precache-manifest.094ad2f9f5995843927248b5e892ac92.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22445c55564ea4764c3ed0b6e587daee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bef06abdab371c85e2a",
+    "url": "/static/css/main.b7eb6c98.chunk.css"
+  },
+  {
+    "revision": "039462bc0c3d9996573e",
+    "url": "/static/js/0.97dc82d4.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/static/js/0.97dc82d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4cf0ea734d2c651bee",
+    "url": "/static/js/3.264c319b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.264c319b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72332738ac1078059cc5",
+    "url": "/static/js/4.b2dfe31f.chunk.js"
+  },
+  {
+    "revision": "59e4a3fed5bba2e1ccfa",
+    "url": "/static/js/5.e3a3f8fd.chunk.js"
+  },
+  {
+    "revision": "33aa3850b738f379bb93",
+    "url": "/static/js/6.7bb031d6.chunk.js"
+  },
+  {
+    "revision": "4bef06abdab371c85e2a",
+    "url": "/static/js/main.a8cf1f21.chunk.js"
+  },
+  {
+    "revision": "46a30b784aeb471ac7a6",
+    "url": "/static/js/runtime-main.46b12e7b.js"
+  },
+  {
+    "revision": "1e0431edbc7a75798fe93df925d292eb",
+    "url": "/static/media/logoHotel.1e0431ed.png"
+  }
+]);
